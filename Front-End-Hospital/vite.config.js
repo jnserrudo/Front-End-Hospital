6f0d97b44d3d4181d9a642414b87d2ee391a77d5
José Nahuel Serrudo <jnserrudo@ucasal.local>
2024-06-03 11:26:49 -0300

    prueba

--- Front-End-Hospital/vite.config.js
+++ Front-End-Hospital/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/assets/',
   build: {
     outDir: '../Front-End-Hospital/dist',
     rollupOptions: {
