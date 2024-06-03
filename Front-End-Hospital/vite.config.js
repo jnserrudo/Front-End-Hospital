363de259752ff4a2f849b9846c94c8c9d3b53ae2
José Nahuel Serrudo <jnserrudo@ucasal.local>
2024-06-02 22:47:26 -0300

    base vite prueba2

--- Front-End-Hospital/vite.config.js
+++ Front-End-Hospital/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:''
-})
+  build: {
+    outDir: '../Front-End-Hospital/Front-End-Hospital/dist', // Cambia esto a la ruta correcta para tu proyecto
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // Puedes cambiar el puerto si es necesario
+  }
+});
